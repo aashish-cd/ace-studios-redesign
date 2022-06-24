918d60d038c2c5bce1dbdf56fb89b98ba3fe392e
aashish-cd <076bct002@ioepc.edu.np>
2022-06-24 23:14:25 +0545

    hero section done

---
+++ components/HomePageComponents/Hero.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Hero = () => {
+  return (
+    <Container className='sectionMargin'>
+      <h1 className='secondary'>
+        Get a dedicated team to deliver a custom application tailored to your
+        business needs
+      </h1>
+      <Image src='https://acestudios.tech/home.jpg' alt='ace studios' />
+    </Container>
+  );
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  h1 {
+    padding: 0 2rem;
+    font-family: 'Avenir';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 33px;
+    line-height: 160%;
+  }
+  img {
+    max-width: 500px;
+    border-radius: 10px;
+  }
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    width: 90%;
+  }
+`;
+const Image = styled.img`
+  width: 80%;
+`;
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useState, useEffect } from 'react';
-import Content from '../components/Content/Content';
+import Content from '../components/HomePageComponents/Content';
+import Hero from '../components/HomePageComponents/Hero';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <>
       <div className={styles.container}>
+        <Hero />
+        <Content
+          heading='website development'
+          text='Grow your brand, improve internal processes, reach emerging markets. Build your web application with our software developers.'
+          image='https://acestudios.tech/webdev.png'
+          imageText='website development'
+          reverse={true}
+        />
+        <Content
+          heading='website development'
+          text='Grow your brand, improve internal processes, reach emerging markets. Build your web application with our software developers.'
+          image='https://acestudios.tech/webdev.png'
+          imageText='website development'
+          reverse={false}
+        />
         <Content
           heading='website development'
           text='Grow your brand, improve internal processes, reach emerging markets. Build your web application with our software developers.'
