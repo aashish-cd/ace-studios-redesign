04a8335173277b1ada5ffd623630a201f23c2945
aashish-cd <076bct002@ioepc.edu.np>
2022-06-25 13:38:04 +0545

    window issue

--- customHooks/useWidth.js
+++ customHooks/useWidth.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 const useWidth = () => {
-  const [width, setWidth] = useState(1024);
+  const [width, setWidth] = useState(window?.innerWidth);
   useEffect(() => {
     const handleWidth = () => setWidth(window.innerWidth);
     window.addEventListener('resize', handleWidth);
